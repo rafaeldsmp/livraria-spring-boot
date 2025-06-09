f7393edb52250b87b135cfead34815b216b07376
Rafael Pino <rafaeltsd@gmail.com>
2025-06-09 12:06:46 -0300

    finalizacao da implementacao de token jwt

--- src/main/java/br/com/library/library/config/ResourceServerConfiguration.java
+++ src/main/java/br/com/library/library/config/ResourceServerConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.library.library.config;
 
+import br.com.library.library.security.JwtCustomAuthenticationFilter;
 import br.com.library.library.security.LoginSocialSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,7 +23,7 @@ public class ResourceServerConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(
-            HttpSecurity http, LoginSocialSuccessHandler successHandler) throws Exception {
+            HttpSecurity http, LoginSocialSuccessHandler successHandler, JwtCustomAuthenticationFilter jwtCustomAuthenticationFilter) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
@@ -39,6 +41,7 @@ public class ResourceServerConfiguration {
                             .successHandler(successHandler);
                 })
                 .oauth2ResourceServer(oauth2rs -> oauth2rs.jwt(Customizer.withDefaults()))
+                .addFilterAfter(jwtCustomAuthenticationFilter, BearerTokenAuthenticationFilter.class)
                 .build();
     }
 
